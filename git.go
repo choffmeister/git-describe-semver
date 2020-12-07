e26357632e993c5e15df1ac5afb93a073c8dfa6d
Christian Hoffmeister <mail@choffmeister.de>
2020-12-07 18:27:46 +0100

    Support branches

--- git.go
+++ git.go
@@ -24,6 +24,11 @@ func GitTagMap(repo git.Repository) (*map[string]string, error) {
 
 // GitDescribe ...
 func GitDescribe(repo git.Repository) (*string, *int, *string, error) {
+	type gitDescribeNode struct {
+		Commit   object.Commit
+		Distance int
+	}
+
 	head, err := repo.Head()
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("unable to find head: %v", err)
@@ -35,24 +40,47 @@ func GitDescribe(repo git.Repository) (*string, *int, *string, error) {
 	}
 	commits, err := repo.Log(&git.LogOptions{
 		From:  head.Hash(),
-		Order: git.LogOrderDefault,
+		Order: git.LogOrderBSF,
 	})
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("unable to get log: %v", err)
 	}
+	state := map[string]gitDescribeNode{}
 	counter := 0
 	tagHash := ""
 	commits.ForEach(func(c *object.Commit) error {
-		_, found := (*tags)[c.Hash.String()]
-		if tagHash == "" && found {
-			tagHash = c.Hash.String()
+		node, found := state[c.Hash.String()]
+		if !found {
+			node = gitDescribeNode{
+				Commit:   *c,
+				Distance: 0,
+			}
+			state[c.Hash.String()] = node
 		}
-		if tagHash == "" {
-			counter = counter + 1
+		c.Parents().ForEach(func(p *object.Commit) error {
+			_, found := state[p.Hash.String()]
+			if !found {
+				state[p.Hash.String()] = gitDescribeNode{
+					Commit:   *p,
+					Distance: node.Distance + 1,
+				}
+			}
+			return nil
+		})
+
+		_, foundTag := (*tags)[c.Hash.String()]
+		if tagHash == "" && foundTag {
+			counter = state[c.Hash.String()].Distance
+			tagHash = c.Hash.String()
 		}
 		return nil
 	})
 	if tagHash == "" {
+		for _, node := range state {
+			if node.Distance+1 > counter {
+				counter = node.Distance + 1
+			}
+		}
 		tagName := ""
 		return &tagName, &counter, &headHash, nil
 	}
--- git_test.go
+++ git_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -58,9 +59,44 @@ func TestGitDescribe(t *testing.T) {
 	commit2, _ := worktree.Commit("second", &git.CommitOptions{})
 	test("v1.0.0", 1, commit2.String())
 
-	commit3, _ := worktree.Commit("second", &git.CommitOptions{})
+	commit3, _ := worktree.Commit("third", &git.CommitOptions{})
 	test("v1.0.0", 2, commit3.String())
 
 	repo.CreateTag("v2.0.0", commit3, nil)
 	test("v2.0.0", 0, commit3.String())
+}
+
+func TestGitDescribeWithBranch(t *testing.T) {
+	assert := assert.New(t)
+	dir, _ := ioutil.TempDir("", "example")
+	repo, _ := git.PlainInit(dir, false)
+	worktree, _ := repo.Worktree()
+	_, _, _, err := GitDescribe(*repo)
+	assert.Error(err)
+	test := func(expectedTagName string, expectedCounter int, expectedHeadHash string) {
+		actualTagName, actualCounter, actualHeadHash, err := GitDescribe(*repo)
+		assert.NoError(err)
+		assert.Equal(expectedTagName, *actualTagName)
+		assert.Equal(expectedCounter, *actualCounter)
+		assert.Equal(expectedHeadHash, *actualHeadHash)
+	}
+
+	commit1, _ := worktree.Commit("first", &git.CommitOptions{})
+	test("", 1, commit1.String())
+
+	repo.CreateTag("v1.0.0", commit1, nil)
+	test("v1.0.0", 0, commit1.String())
+
+	commit2, _ := worktree.Commit("second", &git.CommitOptions{})
+	test("v1.0.0", 1, commit2.String())
+
+	worktree.Checkout(&git.CheckoutOptions{Hash: commit1})
+
+	commit3, _ := worktree.Commit("third", &git.CommitOptions{})
+	test("v1.0.0", 1, commit3.String())
+
+	commit4, _ := worktree.Commit("forth", &git.CommitOptions{Parents: []plumbing.Hash{commit2, commit3}})
+	test("v1.0.0", 2, commit4.String())
+	repo.CreateTag("v2.0.0", commit3, nil)
+	test("v2.0.0", 1, commit4.String())
 }
